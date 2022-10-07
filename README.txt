a8a72f4ec177ca47baba8b1ed3e7be3eef8058dd
AD <92855802+ad0409@users.noreply.github.com>
2022-10-07 13:27:48 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+This is a sample Python script to fetch data from NASA's Mars Rover Photo API
+
+Step by step:
+1) Install all required packages listed in requirements.txt file
+2) Run run.py file
---
+++ run.py
@@ -0,0 +1,20 @@
+import requests
+import logging
+
+
+endpoint = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+# Replace DEMO_KEY below with your own key if you generated one.
+api_key = "DEMO_KEY"
+earth_date = "2022-10-01"
+
+
+query_params = {"api_key": api_key, "earth_date": earth_date}
+response = requests.get(endpoint, params=query_params)
+print(response)
+response.json()
+photos = response.json()["photos"]
+print(f"Found {len(photos)} photos")
+
+#print(photos[1]["img_src"])
+for i in range(0, len(photos)):
+    print(photos[i])
